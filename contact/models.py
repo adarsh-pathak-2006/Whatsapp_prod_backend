383c606cc234dffdab61082b1f337204cec4438d
duckyduck01 <madhvetipathak2006@gmail.com>
2026-07-17 21:51:00 +0530

    added models and added apps in the settings

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    mobile_no=models.CharField(max_length=15)
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo=models.ImageField(upload_to="profile_pics/", null=True, blank=True)
+    bio=models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.user.username
+    
+class Contact(models.Model):
+    name=models.CharField(max_length=200)
+    user=models.ManyToManyField(Profile)
+
+    def __str__(self):
+        return self.name
+
+    
